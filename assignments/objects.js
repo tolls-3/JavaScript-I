e5c13e37a7da940087ac845c8b6ed8c7e7349833
Tolu Atolagbe <tolu.atolagbe@gmail.com>
2019-08-19 13:42:49 +0100

    object.js challenge 3 done

--- assignments/objects.js
+++ assignments/objects.js
@@ -88,8 +88,13 @@ console.log(kennan.speak());
 
 // Antonietta loves math, give her the ability to multiply two numbers together and return the product. Use the console.log provided as a hint.
 //console.log(antonietta.multiplyNums(3,4));
-
-
+let antonietta = {
+  multiplyNums: function(p1, p2){
+    const multiply= p1 * p2;
+    return multiply;
+  }
+}
+console.log(antonietta.multiplyNums(3,4));
 // === Great work! === Head over to the the arrays.js. You may come back and attempt the Stretch Challenge once you have completed the challenges in arrays.js and function-conversion.js.
 
 // ==== Stretch Challenge: Nested Objects and the this keyword ==== 
