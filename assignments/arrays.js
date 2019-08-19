52132d67bd645b816f568a947e659613269b0b28
Tolu Atolagbe <tolu.atolagbe@gmail.com>
2019-08-19 14:21:47 +0100

    array.js challenge 3 done

--- assignments/arrays.js
+++ assignments/arrays.js
@@ -81,13 +81,18 @@ console.log(`Car 33 is a ${inventory[32].car_year} ${inventory[32].car_make} ${i
 // ==== Challenge 2 ====
 // The dealer needs the information on the last car in their inventory.  What is the make and model of the last car in the inventory?  Log the make and model into the console.
 let lastCar = 0;
-console.log();
+console.log(`${inventory[inventory.length-1].car_make } ${inventory[inventory.length-1].car_model }`);
 
 // ==== Challenge 3 ====
 // The marketing team wants the car models listed alphabetically on the website. Sort all the car model names into alphabetical order and log the results in the console
 let carModels = [];
-let carModelsSorted = [];
-console.log();
+for(var i=0; i<inventory.length;i++){
+    let carModelsSorted = inventory[i].car_model;
+    carModels.push(carModelsSorted);
+    carModels.sort();
+    console.log(carModels);
+}
+
 
 // ==== Challenge 4 ====
 // The accounting team needs all the years from every car on the lot. Create a new array from the dealer data containing only the car years and log the result in the console.
